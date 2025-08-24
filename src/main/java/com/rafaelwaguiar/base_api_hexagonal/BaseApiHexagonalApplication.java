65a37704f079a0a573bb87f734c8beb8a8d05749
rafaelwaguiar <rafael.aguiar@neogrid.com>
2025-08-24 14:00:50 -0300

    Comit Inicial - Abertura do projeto base

---
+++ src/main/java/com/rafaelwaguiar/base_api_hexagonal/BaseApiHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelwaguiar.base_api_hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseApiHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseApiHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelwaguiar/base_api_hexagonal/BaseApiHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelwaguiar.base_api_hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseApiHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
